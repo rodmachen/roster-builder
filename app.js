0d16eba0ba60b5012d7d647fa282a76ef7ebecbe
Rod Machen <mail@rodmachen.com>
2015-09-22 08:37:30 -0500

    Implement local storage, with default data

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ angular.module('roster', [
   "ngStorage"
 ])
 .controller('MainCtrl', function($scope, $localStorage) {
-  // $scope.players = [
   $scope.$storage = $localStorage;
-  $scope.$storage = [{num: 09, last: 'Brees', first: 'Drew', pos: 'QB'}];
-  // $scope.$storage = [
-  //   // {num: 09, last: 'Brees', first: 'Drew', pos: 'QB'},
-  //   // {num: 1, last: 'Murray', first: 'Kyler', pos: 'QB'},
-  //   // {num: 2, last: 'Noil', first: 'Speedy', pos: 'WR'},
-  //   // {num: 3, last: 'Kirk', first: 'Christian', pos: 'WR'},
-  // ];
-  // $scope.players = $scope.$storage;
+  if ($scope.$storage.players.length === 0) {
+    localStorage.clear();
+    $scope.$storage.players = [
+      {num: 09, last: 'Brees', first: 'Drew', pos: 'QB'}
+      // {num: 1, last: 'Murray', first: 'Kyler', pos: 'QB'},
+      // {num: 2, last: 'Noil', first: 'Speedy', pos: 'WR'},
+      // {num: 3, last: 'Kirk', first: 'Christian', pos: 'WR'}''
+    ];
+  }
   $scope.addPlayer = function() {
-    $scope.$storage.push({num: $scope.num, last: $scope.last, first: $scope.first, pos: $scope.pos});
+    $scope.$storage.players.push({num: $scope.num, last: $scope.last, first: $scope.first, pos: $scope.pos});
     $scope.num = '';
     $scope.last = '';
     $scope.first = '';
@@ -21,12 +21,11 @@ angular.module('roster', [
   };
 
   $scope.removePlayer = function(player) {
-    for (var i = 0; i < $scope.$storage.length; i++) {
-      if ($scope.$storage[i] === player) {
-        $scope.$storage.splice(i, 1);
+    for (var i = 0; i < $scope.$storage.players.length; i++) {
+      if ($scope.$storage.players[i] === player) {
+        $scope.$storage.players.splice(i, 1);
       }
     }
-
-    // var index = $scope.players.indexOf(item);
   };
+
 });
