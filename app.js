af0168f543a00299110c19172f6b4a6e7a462874
Rod Machen <mail@rodmachen.com>
2015-09-22 17:23:16 -0500

    Finish Firebase implementation

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-angular.module('roster', [
-  'ngStorage',
-])
-.controller('MainCtrl', function($scope, $localStorage) {
-  $scope.$storage = $localStorage;
-  if (!$scope.$storage.players || $scope.$storage.players.length === 0) {
-    localStorage.clear();
-    $scope.$storage.players = [
-      {num: 10, last: 'Allen', first: 'Kyle', pos: 'QB'},
-      {num: 9, last: 'Brees', first: 'Drew', pos: 'QB'},
-      // {num: 01, last: 'Murray', first: 'Kyler', pos: 'QB'},
-      // {num: 02, last: 'Noil', first: 'Speedy', pos: 'WR'},
-      // {num: 03, last: 'Kirk', first: 'Christian', pos: 'WR'}''
-    ];
-  }
+angular.module('roster', ['ngStorage','firebase',])
+.controller('MainCtrl', function($scope, $localStorage, $firebaseArray) {
+
+  var players = $firebaseArray(new Firebase('https://torrid-torch-9549.firebaseio.com/'));
+  $scope.players = players;
+  $scope.players.$loaded().then(function(players) {
+    if (!$scope.players.length) {
+      $scope.players.$add({num: 01, last: 'Murray', first: 'Kyler', pos: 'QB'});
+      $scope.players.$add({num: 10, last: 'Allen', first: 'Kyle', pos: 'QB'});
+      // $scope.players.$add({num: 01, last: 'Murray', first: 'Kyler', pos: 'QB'};
+      // $scope.players.$add({num: 02, last: 'Noil', first: 'Speedy', pos: 'WR'};
+      // $scope.players.$add({num: 03, last: 'Kirk', first: 'Christian', pos: 'WR'};
+      // $scope.players.$add({num: 9, last: 'Brees', first: 'Drew', pos: 'QB'};
+    }
+  });
 
   $scope.sortType     = 'num';
   $scope.sortReverse  = false;
 
   $scope.addPlayer = function() {
-    $scope.$storage.players.push({num: $scope.num, last: $scope.last, first: $scope.first, pos: $scope.pos});
+    $scope.players.$add({num: $scope.num, last: $scope.last, first: $scope.first, pos: $scope.pos});
     $scope.num = '';
     $scope.last = '';
     $scope.first = '';
@@ -26,9 +26,9 @@ angular.module('roster', [
   };
 
   $scope.removePlayer = function(player) {
-    for (var i = 0; i < $scope.$storage.players.length; i++) {
-      if ($scope.$storage.players[i] === player) {
-        $scope.$storage.players.splice(i, 1);
+    for (var i = 0; i < $scope.players.length; i++) {
+      if ($scope.players[i] === player) {
+        $scope.players.$remove(i);
       }
     }
   };
