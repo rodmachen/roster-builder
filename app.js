65f11074798491faf7462cdb46867403219971e7
Rod Machen <mail@rodmachen.com>
2015-09-22 13:19:46 -0500

    Clean up sort text

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 angular.module('roster', [
-  "ngStorage"
+  'ngStorage',
 ])
 .controller('MainCtrl', function($scope, $localStorage) {
-  $scope.sortType     = 'num'; // set the default sort type
-  $scope.sortReverse  = false;  // set the default sort order
-  $scope.searchFish   = ''; 
-
   $scope.$storage = $localStorage;
   if (!$scope.$storage.players || $scope.$storage.players.length === 0) {
     localStorage.clear();
     $scope.$storage.players = [
-      {num: 09, last: 'Brees', first: 'Drew', pos: 'QB'},
-      {num: 01, last: 'Murray', first: 'Kyler', pos: 'QB'},
+      {num: 10, last: 'Allen', first: 'Kyle', pos: 'QB'},
+      {num: 9, last: 'Brees', first: 'Drew', pos: 'QB'},
+      // {num: 01, last: 'Murray', first: 'Kyler', pos: 'QB'},
       // {num: 02, last: 'Noil', first: 'Speedy', pos: 'WR'},
       // {num: 03, last: 'Kirk', first: 'Christian', pos: 'WR'}''
     ];
   }
+
+  $scope.sortType     = 'num';
+  $scope.sortReverse  = false;
+
   $scope.addPlayer = function() {
     $scope.$storage.players.push({num: $scope.num, last: $scope.last, first: $scope.first, pos: $scope.pos});
     $scope.num = '';
